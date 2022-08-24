af4d6126699f08da98c4efc771302ab359b638c6
Abhinav Pandey <pandeyabhinav2608@gmail.com>
2022-08-24 16:56:19 +0530

    written all the methods and handled the ArithmeticException

--- src/main/java/com/jap/calculator/MathematicalCalculator.java
+++ src/main/java/com/jap/calculator/MathematicalCalculator.java
@@ -2,26 +2,26 @@ package com.jap.calculator;
 
 public class MathematicalCalculator {
 
-	public int add(int num1, int num2)
-	{	
-		return -1;
+	public int add(int num1, int num2) {
+		return num1 + num2;
 	}
-	public int subtract(int num1, int num2)
-	{	
-		return -2;
+	public int subtract(int num1, int num2) {
+		return num1 - num2;
 	}
-	public int multiply(int num1, int num2)
-	{	
-		return -1;
+	public int multiply(int num1, int num2) {
+		return num1 * num2;
 	}
-	public int divide(int num1, int num2)
-	{
+	public int divide(int num1, int num2) {
 		//write the code and handle the ArithmeticException
-         return -1;
-
+		try{
+			int  result = num1 / num2;
+			return result;
+		}catch(ArithmeticException e){
+			int result = 0;
+			return result;
+		}
 	}
-	public int modulo(int num1, int num2)
-	{	
-		return-1;
+	public int modulo(int num1, int num2) {
+		return num1 % num2;
 	}
 }
